f1052be72d72da1b0191df37d550b504ddfbf235
yohana.espinoza <yohana.espinoza@skygenusa.com>
2018-07-18 17:42:50 -0500

    Exercise 5 - Movee variables at the top

--- CleanCode/05 VariableDeclarationsAtTheTop/PayCalculator.cs
+++ CleanCode/05 VariableDeclarationsAtTheTop/PayCalculator.cs
@@ -14,11 +14,7 @@ namespace CleanCode.VariableDeclarationsAtTheTop
         {
             decimal overtimeHours = 0;
             decimal regularHours = 0;
-            decimal regularPay = 0;
-            decimal overtimePay = 0;
-
-            decimal grossPay = 0;
-
+            
             if (_payFrequency == PayFrequency.Fortnightly)
             {
                 if (hours > 80)
@@ -29,8 +25,7 @@ namespace CleanCode.VariableDeclarationsAtTheTop
                 else
                     regularHours = hours;
             }
-
-
+            
             else if (_payFrequency == PayFrequency.Weekly)
             {
                 if (hours > 40)
@@ -42,11 +37,15 @@ namespace CleanCode.VariableDeclarationsAtTheTop
                     regularHours = hours;
             }
 
+            decimal overtimePay = 0;
 
             if (overtimeHours > 0m)
             {
                 overtimePay += (rate * 1.5m) * overtimeHours;
             }
+
+            decimal regularPay = 0;
+            decimal grossPay = 0;
 
             regularPay = (regularHours * rate);
             grossPay = regularPay + overtimePay;
